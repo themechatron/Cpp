3f5c877abd9e3a6def6a06cfdd2431b71fe0f641
zaki1993 <zpetrov96@gmail.com>
2016-10-13 13:42:38 +0300

    Singleton

---
+++ singleton.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+/*
+class Point{
+	double x, y;
+public:
+	Point(double _x, double _y) : x(_x), y(_y){}
+	static Point getZero(){
+		return Point(0, 0);
+	}
+	void doStuff(){
+		Point zero = getZero();
+		std::cout << "Stuff" << std::endl;
+	}
+};
+
+class A{
+	static int x[];
+public:
+	int y;
+	int &operator[](size_t ind){ return x[ind]; }
+};
+int A::x[] = { 1, 2, 3 };
+*/
+
+class Singleton{
+	static Singleton s;
+	int a, b;
+	Singleton(int x, int y):a(x),b(y){}
+public:
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+	static int getA(){
+		return s.a;
+	}
+	static void setA(int _a){
+		s.a = _a;
+	}
+};
+
+Singleton Singleton::s(1, 2);
+int main(){
+	/*
+	Point p1(2, 5);
+	p1.doStuff();
+
+	Point p2 = p1.getZero();
+	Point p3 = Point::getZero();
+	std::cout << sizeof(A) << std::endl;
+	A a1;
+	std::cout << a1[0] << std::endl;
+	*/
+
+	//Singleton
+	std::cout << Singleton::getA() << " " << std::endl;
+	system("pause");
+	return 0;
+}
