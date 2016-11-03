593da7ec9a09d8469a4df1fe2861c6f7aa8e30f6
Zaki Petrov <Zaki Petrov>
2016-11-03 16:58:28 -0700

    lambda expressions

--- Functions/lambda_expressions.cpp
+++ Functions/lambda_expressions.cpp
@@ -8,12 +8,6 @@ auto mult = [](int x, int y){ return x * y; };
 auto devide = [](int x, int y){ return x / y; };
 //c++ 14 [](auto x, auto y){..}
 
-
-template<class T,class U>
-auto asd = [](T x, U y) const{ return x + y; };
 int main(){
-
-	cout << asd(3, 4);
-	system("pause");
 	return 0;
 }
