d37c555773286f2bfd1ddb9a1b3d2aa4fff56d4d
Zaki Petrov <Zaki Petrov>
2016-12-13 15:23:22 -0800

    Add some more methods

--- BSTree/BTree.cpp
+++ BSTree/BTree.cpp
@@ -36,11 +36,6 @@ void BTree<T>::remove(const T& _key){
 }
 
 template<class T>
-void BTree<T>::print() const{
-	privatePrint(root);
-}
-
-template<class T>
 size_t BTree<T>::count() const{
 	return size;
 }
@@ -83,8 +78,72 @@ int BTree<T>::maxLeave() const{
 }
 
 template<class T>
-T& BTree<T>::getElement(const char* s){
+T& BTree<T>::getElement(const char* s) const{
 	return privateGetElement(root, s);
+}
+
+template<class T>
+void BTree<T>::inOrder() const{
+	privateInOrder(root);
+}
+
+template<class T>
+void BTree<T>::preOrder() const{
+	privatePreOrder(root);
+}
+
+template<class T>
+void BTree<T>::postOrder() const{
+	privatePostOrder(root);
+}
+
+template<class T>
+vector<T> BTree<T>::listLeaves() const{
+	vector<T> leaves;
+	privateListLeaves(root, leaves);
+	return leaves;
+}
+
+template<class T>
+bool BTree<T>::contains(const T& element) const{
+	bool result = false;
+	privateContains(root, element, result);
+	return result;
+}
+
+template<class T>
+string BTree<T>::findTrace(const T& element) const{
+	if (!contains(element)){
+		return "_";
+	}
+	string result = "";
+	privateFindTrace(root, result, element);
+	return result;
+}
+
+template<class T>
+void BTree<T>::prettyPrint() const{
+	privatePrettyPrint(root, 0);
+}
+
+template<class T>
+void BTree<T>::serealize(string filename) const{
+	ofstream file(filename.c_str());
+	privateSerealize(file, root);
+}
+
+template<class T>
+T& BTree<T>::operator[](int index) const{
+	T result;
+	privateOperatorFind(root, index, result,1);
+	return result;
+}
+
+template<class T>
+vector<T> BTree<T>::level(int k) const{
+	vector<T> result;
+	privateLevel(root, result, k, 0);
+	return result;
 }
 
 template<class T>
@@ -94,7 +153,7 @@ BTree<T>::~BTree(){
 
 //private methods
 template<class T>
-auto BTree<T>::navigate(const T& _key, Node*& _root) -> Node*&{
+auto BTree<T>::navigate(const T& _key, Node*& _root) const -> Node*&{
 	if (_root == nullptr || _root->key == _key){
 		return _root;
 	}
@@ -104,16 +163,6 @@ auto BTree<T>::navigate(const T& _key, Node*& _root) -> Node*&{
 	if (_root->key > _key){
 		return navigate(_key, _root->right);
 	}
-}
-
-template<class T>
-void BTree<T>::privatePrint(Node* _root) const{
-	if (_root == nullptr){
-		return;
-	}
-	privatePrint(_root->left);
-	cout << _root->key << " ";
-	privatePrint(_root->right);
 }
 
 template<class T>
@@ -173,7 +222,7 @@ int BTree<T>::privateMaxLeave(Node* _root, int max) const{
 }
 
 template<class T>
-T& BTree<T>::privateGetElement(Node* _root, const char* s){
+T& BTree<T>::privateGetElement(Node* _root, const char* s) const{
 	if (_root == nullptr){
 		cout << "No element match!" << endl;
 		return _root->key;
@@ -205,4 +254,132 @@ void BTree<T>::freeNode(Node* _root){
 	freeNode(_root->left);
 	freeNode(_root->right);
 	delete _root;
+}
+
+template<class T>
+void BTree<T>::privateInOrder(Node* _root) const{
+	if (_root == nullptr){
+		return;
+	}
+	privateInOrder(_root->left);
+	cout << _root->key << " ";	
+	privateInOrder(_root->right);
+}
+
+template<class T>
+void BTree<T>::privatePreOrder(Node* _root) const{
+	if (_root == nullptr){
+		return;
+	}
+	cout << _root->key << " ";
+	privatePreOrder(_root->left);
+	privatePreOrder(_root->right);
+}
+
+template<class T>
+void BTree<T>::privatePostOrder(Node* _root) const{
+	if (_root == nullptr){
+		return;
+	}
+	privatePostOrder(_root->left);
+	privatePostOrder(_root->right);
+	cout << _root->key << " ";
+}
+
+template<class T>
+void BTree<T>::privateListLeaves(Node* _root, vector<T>& leaves) const{
+	if (_root != nullptr && _root->left == nullptr && _root->right == nullptr){
+		leaves.push_back(_root->key);
+	}
+	if (_root == nullptr){
+		return;
+	}
+	privateListLeaves(_root->left, leaves);
+	privateListLeaves(_root->right, leaves);
+}
+
+template<class T>
+void BTree<T>::privateContains(Node* _root, const T& element, bool& result) const{
+	if (_root == nullptr){
+		return;
+	}
+	if (_root->key == element){
+		result = true;
+		return;
+	}
+	privateContains(_root->left, element, result);
+	privateContains(_root->right, element, result);
+}
+
+template<class T>
+void BTree<T>::privateFindTrace(Node* _root, string& trace, const T& number) const{
+	if (_root == nullptr || _root->key == number){
+		return;
+	}
+	if (_root->key < number){
+		trace += "L";
+		privateFindTrace(_root->left, trace, number);
+	}
+	else{
+		trace += "R";
+		privateFindTrace(_root->right, trace, number);
+	}
+}
+
+template<class T>
+void BTree<T>::privatePrettyPrint(Node* _root, size_t position) const{
+	if (_root == nullptr){
+		return;
+	}
+	privatePrettyPrint(_root->left, ++position);
+	for (size_t i = 0; i < position; i++)
+	{
+		cout << "\t";
+	}
+	cout << _root->key << endl;
+	privatePrettyPrint(_root->right, ++position);
+}  
+
+template<class T>
+void BTree<T>::privateSerealize(ofstream& file, Node* _root) const{
+	if (_root == nullptr){
+		file << "()";
+		return;
+	}
+	file << "(" << _root->key;
+	privateSerealize(file, _root->left);
+	file << ")";
+	privateSerealize(file, _root->right);
+	file << ")";
+}
+
+template<class T>
+void BTree<T>::privateOperatorFind(Node* _root, int index, T& result, int previous) const {
+	if (index == 0 && _root != nullptr){
+		result = _root->key;
+		return;
+	}
+	if (_root == nullptr){
+		result = root->key;
+		return;
+	}
+	if (previous == 1){
+		privateOperatorFind(_root->left, index - 1, result, previous + 1);
+	}
+	if (previous == 2){
+		privateOperatorFind(_root->left, index - 1, result, previous - 1);
+	}
+}
+
+template<class T>
+void BTree<T>::privateLevel(Node* _root, vector<T>& result, int k, int current) const{
+	if (_root == nullptr || k < 0){
+		return;
+	}
+	if (current == k){
+		result.push_back(_root->key);
+		return;
+	}
+	privateLevel(_root->left, result, k, current + 1);
+	privateLevel(_root->right, result, k, current + 1);
 }
--- BSTree/BTree.h
+++ BSTree/BTree.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+
 using std::cout;
 using std::endl;
 using std::max;
+using std::vector;
+using std::string;
+using std::ofstream;
+using std::ifstream;
 
 template<class T>
 class BTree{
@@ -22,23 +30,42 @@ public:
 	BTree<T>& operator=(const BTree<T>&) = delete;
 	void add(const T&);
 	void remove(const T&);
-	void print() const;
 	size_t count() const;
 	size_t evens() const;
 	size_t searchCount(bool(*)(const T&)) const;
 	size_t height() const;
 	size_t leaves() const;
 	int maxLeave() const;
-	T& getElement(const char*);
+	T& getElement(const char*) const;
+	void inOrder() const;
+	void preOrder() const;
+	void postOrder() const;
+	vector<T> listLeaves() const;
+	bool contains(const T&) const;
+	string findTrace(const T&) const;
+	void prettyPrint() const;
+	void serealize(string) const;
+	T& operator[](int) const;
+	vector<T> level(int) const;
 	~BTree();
 private:
-	Node*& navigate(const T&, Node*&);
+	Node*& navigate(const T&, Node*&) const;
 	void privatePrint(Node*) const;
 	void privateEvens(Node*, size_t&) const;
 	void privateSearchCount(bool(*)(const T&), Node*, size_t&) const;
 	size_t privateHeight(Node*) const;
 	void privateLeaves(Node*, size_t&) const;
 	int privateMaxLeave(Node*, int) const;
-	T& privateGetElement(Node*, const char*);
+	T& privateGetElement(Node*, const char*) const;
+	void privateInOrder(Node*) const;
+	void privatePreOrder(Node*) const;
+	void privatePostOrder(Node*) const;
+	void privateListLeaves(Node*, vector<T>&) const;
+	void privateContains(Node*, const T&, bool&) const;
+	void privateFindTrace(Node*, string&, const T&) const;
+	void privatePrettyPrint(Node* root, size_t position) const;
+	void privateSerealize(ofstream&, Node*) const;
+	void privateOperatorFind(Node*, int, T&, int) const;
+	void privateLevel(Node*,vector<T>& , int, int) const;
 	void freeNode(Node*);
 };
