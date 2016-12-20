a41332866edc6c0362069f7bebdfa3609c1de31f
zaki1993 <zaki1993@abv.bg>
2016-12-20 11:20:40 -0800

    Fix insert function

--- SkipList/SkipList.h
+++ SkipList/SkipList.h
@@ -1,20 +1,37 @@
-#pragma once
-#define MAX_HEIGHT 32
+#include <iostream>
+#include<stdlib.h>
+#include<ctime>
+
+using namespace std;
+
+#define MAX_LEVEL 32
+#define PROB 0.25
+
 template<class T>
-class SkipList
-{
+class SkipList{
 private:
 	struct Node{
-		T* value;
-		Node* next;
-		Node* previous;
+		T data;
+		Node* left, *right, *up, *down;
+		Node(const T& da = T(), Node*l = nullptr, Node* r = nullptr, Node* u = nullptr, Node* d = nullptr)
+			:data(da), left(l), right(r), up(u), down(d){}
 	};
-	int height;
 	Node* head;
+	Node* tail;
+	int level;
 public:
-	SkipList(int, Node*);
-	SkipList(const SkipList<T>&) = delete;
-	SkipList<T>& operator=(const SkipList&) = delete;
-	~SkipList();
+	SkipList(int _level = 1);
+	void insert(const T& value);
+	void print() const;
+	bool contains(const T& value);
+private:
+	double drand(){
+		return rand() / (RAND_MAX + 1.0);
+	}
+	int generateLevel(){
+		int _level;
+		for (_level = 1; drand() < PROB && _level < level; _level++){
+		}
+		return _level;
+	}
 };
-
