8122a0a3f2cbd928fbbf11e274a09700b305f970
zaki1993 <zaki1993@abv.bg>
2016-12-21 12:20:06 -0800

    Add more tests

--- SkipList/main.cpp
+++ SkipList/main.cpp
@@ -1,8 +1,7 @@
 #include "SkipList.h"
 
-int main()
-{
-	cout << std::boolalpha;
+int main(){
+cout << std::boolalpha;
 	srand(time(NULL));
 	SkipList<int> zaki;
 	for (int i = -100; i<100; i++){
@@ -35,7 +34,57 @@ int main()
 	SkipList<int> gosho;
 	gosho = pesho;
 	gosho.print();
-	cout << gosho.contains(99) << endl;
-  
+	cout << pesho.contains(99) << endl;
+	cout << gosho.contains(10000) << endl;
+
+
+	SkipList<int> merge1;
+	for (size_t i = 0; i < 16; i++)
+	{
+		merge1.insert(i);
+	}
+	SkipList<int> merge2;
+	for (size_t i = 15; i < 20; i++)
+	{
+		merge2.insert(i);
+	}
+
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge1.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge2.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+
+	SkipList<int> aaa = merge1.merge(merge2);
+
+
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge1.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge2.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+
+	cout << "--------------------" << endl;
+	aaa.print();
+	aaa.insert(100000);
+	aaa.remove(5);
+	cout << endl;
+	aaa.print();
+	cout << "--------------------" << endl;
+	cout << endl;
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge1.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	merge2.print();
+	cout << "lllllllllllllLLLLLLLLLLLLLLLLLLLLLLlllllllllll" << endl;
+	cout << "INTERSECT" << endl;
+	SkipList<int> bbb = merge2.intersect(merge1);
+	bbb.print();
+	bbb.insert(9999);
+	bbb.remove(6);
+	cout << "==========" << endl;
+	bbb.print();
+	cout << "--------------------" << endl;
+	
 	return 0;
 }
