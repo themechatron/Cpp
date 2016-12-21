e0a386de9de06d5b26dcd659ce656a096ee66776
zaki1993 <zaki1993@abv.bg>
2016-12-20 16:39:20 -0800

    Add some functions
    Fixed increase, contains functions. Added remove and all the constructors + desctructor + some helper functions.

--- SkipList/SkipList.h
+++ SkipList/SkipList.h
@@ -6,24 +6,29 @@ using namespace std;
 
 #define MAX_LEVEL 32
 #define PROB 0.25
-
+;
 template<class T>
 class SkipList{
 private:
 	struct Node{
 		T data;
 		Node* left, *right, *up, *down;
-		Node(const T& da = T(), Node*l = nullptr, Node* r = nullptr, Node* u = nullptr, Node* d = nullptr)
-			:data(da), left(l), right(r), up(u), down(d){}
+		Node(const T& _data = T(), Node* _left = nullptr, Node* _right = nullptr, Node* _up = nullptr, Node* _down = nullptr)
+			:data(_data), left(_left), right(_right), up(_up), down(_down){}
 	};
 	Node* head;
 	Node* tail;
 	int level;
+	int elements;
 public:
 	SkipList(int _level = 1);
+	SkipList(const SkipList<T>& other);
+	SkipList<T>& operator=(const SkipList<T>& other);
 	void insert(const T& value);
 	void print() const;
 	bool contains(const T& value);
+	void remove(const T& value);
+	~SkipList();
 private:
 	double drand(){
 		return rand() / (RAND_MAX + 1.0);
@@ -34,4 +39,6 @@ private:
 		}
 		return _level;
 	}
+	void copy(Node* _head, Node* _tail, int _level, int _elements);
+	void freeNodes(Node* _head, Node* _tail);
 };
